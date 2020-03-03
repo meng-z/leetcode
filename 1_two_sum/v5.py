1a211f709cf4cba515b62210c15e114313b3a9ca
Meng Zhao <mzhao@yahoo-corp.jp>
2020-03-04 01:02:38 +0900

    update 1

--- 1_two_sum/v5.py
+++ 1_two_sum/v5.py
@@ -1,8 +1,6 @@
 '''
 Runtime: 48 ms, faster than 78.67% of Python3 online submissions for Two Sum.
 Memory Usage: 14.2 MB, less than 54.18% of Python3 online submissions for Two Sum.
-
-The idea is the same as that of v4, just for a review.
 '''
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
